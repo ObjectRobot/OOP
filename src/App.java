0e22e19c480d6e050e966cf01389341f65eb18cd
Oliver Kokolee <ollie12245@gmail.com>
2022-09-06 22:40:35 -0400

    September 6, 2022

--- src/App.java
+++ src/App.java
@@ -1,9 +1,21 @@
+import javax.sound.sampled.SourceDataLine;
+
 public class App {
     public static void main(String[] args) throws Exception {
         // Abstraction is to work with something we know how to use without
         // knowing how it works internally.
         Student student = new Student("Oliver", "Kokolee", "CHC");
         System.out.println(student.greeting());
+
+        // Write the lines of code to create an UndergradStudent object
+        // using the student object as the basis for creation.
+        // call the greeting, creditsRemaining, and isMajorComplete methods
+        // on the UndergradStudent object.
+        UndergradStudent undergrad = new UndergradStudent(student.getFirstName(), student.getLastName(), student.getSchoolName(), gpa, creditsEarned, major); // 3.75, 40, Computer Science
+            System.out.println(undergrad.greeting());
+            System.out.println(undergrad.creditsRemaining(creditsRequired)); // 40
+            System.out.println(undergrad.isMajorComplete(creditsRequired)); // 40
+        
 
         Integer one = 1;
         Double two = 2.0;
